56944d953ffb2555cf7549608c225f261f4b5a75
aylmer1978 <aylmer1978@gmail.com>
2026-06-20 20:43:30 +0200

    little change

--- main.cpp
+++ main.cpp
@@ -9,9 +9,11 @@ int main() {
   char comando = ' ';
 
   std::cout << "Escribe 'd' o 'i'" << std::endl;
-  
-  std::cout << contenido.substr(0, cursor) << "|" << contenido.substr(cursor, contenido.length()-cursor) << std::endl;
-  
+
+  std::cout << contenido.substr(0, cursor) << "|"
+            << contenido.substr(cursor, contenido.length() - cursor)
+            << std::endl;
+
   while (comando != 'q') {
 
     std::cin >> comando;
@@ -24,31 +26,30 @@ int main() {
     } else if (comando == 'i') {
       if (cursor > 0) {
         cursor--;
-        
       }
 
     } else if (comando == 'b') {
-      if (cursor >0) {
-        contenido.erase(cursor-1, 1);
+      if (cursor > 0) {
+        contenido.erase(cursor - 1, 1);
         cursor--;
-        }
+      }
 
     } else if (comando == 'q') {
       break;
-    
+
     } else if (comando == 'x') {
       if (cursor < contenido.length()) {
         contenido.erase(cursor, 1);
-      } 
+      }
 
     } else {
       contenido.insert(cursor, 1, comando);
       cursor++;
-
     }
 
-    std::cout << contenido.substr(0, cursor) << "|" << contenido.substr(cursor, contenido.length()-cursor) << std::endl;
-
+    std::cout << contenido.substr(0, cursor) << "|"
+              << contenido.substr(cursor, contenido.length() - cursor)
+              << std::endl;
   }
 
   return 0;
