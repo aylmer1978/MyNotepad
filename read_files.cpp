b23ddde1680ca74083cdc1e019b342fe5f659282
aylmer1978 <aylmer1978@gmail.com>
2026-06-21 23:34:32 +0200

    read files

---
+++ read_files.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+
+int main () {
+
+    fs::path my_path = fs::current_path();
+    std::cout << my_path << std::endl;
+
+    for (auto& entry : fs::directory_iterator{my_path}) {
+        std::cout << entry << std::endl;
+    }
+
+
+    return 0;
+}
