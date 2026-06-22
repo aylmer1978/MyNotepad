2e5d481c170a3d8e5a0fd7ba5a6c621b2025b9df
aylmer1978 <aylmer1978@gmail.com>
2026-06-22 15:40:36 +0200

    update read files

--- read_files.cpp
+++ read_files.cpp
@@ -1,15 +1,34 @@
-#include <iostream>
-#include <filesystem>
+// #include <iostream>
+// #include <filesystem>
 
-namespace fs = std::filesystem;
+// namespace fs = std::filesystem;
+
+// int main () {
+
+//     fs::path my_path = fs::current_path();
+//     std::cout << my_path << std::endl;
+
+//     for (auto& entry : fs::directory_iterator{my_path}) {
+//         std::cout << entry << std::endl;
+//     }
+
+//     return 0;
+// }
+//
+
+#include <filesystem>
+#include <iostream>
+#include <string>
 
 int main () {
 
-    fs::path my_path = fs::current_path();
-    std::cout << my_path << std::endl;
-
-    for (auto& entry : fs::directory_iterator{my_path}) {
-        std::cout << entry << std::endl;
+    for (const auto &entrada : std::filesystem::directory_iterator(".")) {
+        std::string file = entrada.path().string();
+        if (file.length() >= 3) {
+            if (file.substr(file.length()-4) == ".txt") {
+                std::cout << file << std::endl;
+            }
+        }
     }
 
     return 0;
